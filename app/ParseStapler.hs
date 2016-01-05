e0d7feeb76cd6fa68d4de27835fc6edbe0282ab7
Andrey Vyatkin <avyatkin@spbtv.com>
2016-01-05 21:46:57 +0300

    implement simple parser

--- app/ParseStapler.hs
+++ app/ParseStapler.hs
@@ -1,9 +1,9 @@
 module ParseStapler where
 
 --import System.IO
---import Control.Monad
---import Text.ParserCombinators.Parsec
---import Text.ParserCombinators.Parsec.Expr
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
@@ -11,7 +11,11 @@ import qualified Text.ParserCombinators.Parsec.Token as Token
 data AExpr = Seq [AExpr] | IntConst Integer | ABinary ABinOp AExpr AExpr deriving (Show)
 data ABinOp = Add | Subtract | Multiply | Divide deriving (Show)
 
-languageDef = emptyDef { Token.reservedOpNames = ["+", "-", "*", "%"] }
+reservedBinOps   = ["+", "-", "*", "%"]
+reservedBinOpAdt = [Add, Subtract, Multiply, Divide]
+binopToAdt = zip reservedBinOps reservedBinOpAdt
+
+languageDef = emptyDef { Token.reservedOpNames = reservedBinOps }
 lexer = Token.makeTokenParser languageDef
 
 reservedOp = Token.reservedOp lexer
@@ -19,3 +23,14 @@ parens     = Token.parens     lexer
 integer    = Token.integer    lexer
 semi       = Token.semi       lexer
 whiteSpace = Token.whiteSpace lexer
+
+staplerParser :: Parser AExpr
+staplerParser = whiteSpace >> aExpression
+
+aExpression = buildExpressionParser aOperators aTerm
+
+aOperators = let binOpParser (op, datatype) = Infix (reservedOp op >> return (ABinary datatype)) AssocLeft
+                 binOps = map binOpParser binopToAdt
+             in [binOps]
+
+aTerm = parens aExpression <|> liftM IntConst integer
